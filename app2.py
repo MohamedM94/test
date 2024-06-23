bb23fe1e05ce00b7f33b8ca109ad73dfd068f580
MohamedM94 <166175173+MohamedM94@users.noreply.github.com>
2024-06-23 20:57:28 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -231,7 +231,7 @@ def main() :
 #Feature importance / description
     if st.checkbox("Affichage des dossiers similaires ?",key="Option3"):
        similar_id = load_neighbors1(X_test,id_client)
-       st.write( ", similar_id)
+       st.write( "", similar_id)
 
     
     else:
